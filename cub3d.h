1330f5f3ec2006638d6f78bf4af90f1063145ae2
rshaheen <rshaheen@student.codam.nl>
2025-01-15 15:19:48 +0100

    file parsing added

--- cub3d.h
+++ cub3d.h
@@ -6,15 +6,17 @@
 /*   By: rshaheen <rshaheen@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/01/15 11:54:45 by rshaheen      #+#    #+#                 */
-/*   Updated: 2025/01/15 12:07:11 by rshaheen      ########   odam.nl         */
+/*   Updated: 2025/01/15 14:56:40 by rshaheen      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 
-// # include "../libft/libft.h"
+# include "libft/libft.h"
+# include <stdbool.h>
 
+bool	check_file_extension(char *str);
 
 
 #endif
