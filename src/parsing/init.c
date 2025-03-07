04c96f2753f30a1c82e16a1b2c4d87f197741507
rshaheen <rshaheen@student.codam.nl>
2025-03-07 13:04:55 +0100

    refractored init

--- src/parsing/init.c
+++ src/parsing/init.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   init.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: kziari <kziari@student.42.fr>              +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/20 15:06:52 by rshaheen          #+#    #+#             */
-/*   Updated: 2025/02/27 15:24:28 by kziari           ###   ########.fr       */
+/*                                                        ::::::::            */
+/*   init.c                                             :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: rshaheen <rshaheen@student.42.fr>            +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2025/01/20 15:06:52 by rshaheen      #+#    #+#                 */
+/*   Updated: 2025/03/07 13:03:16 by rshaheen      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 void	init_config(t_game *game)
 {
+	game->ea = NULL;
+	game->we = NULL;
 	game->no = NULL;
 	game->so = NULL;
-	game->we = NULL;
-	game->ea = NULL;
-	game->floor_color = NULL;
 	game->ceiling_color = NULL;
+	game->floor_color = NULL;
 	game->texture = NULL;
 	game->map = NULL;
 }
